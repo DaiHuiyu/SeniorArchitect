5a635156666d1f19ed22c83783480bd7975a3cc3
dhy <1461870217@qq.com>
2025-01-04 20:04:35 +0800

    更新target

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello world!");
+        Boolean b = 3 * 0.1  == 0.3;
+        System.out.println("3 * 0.1  == 0.3 ：" + b);
+    }
+}
